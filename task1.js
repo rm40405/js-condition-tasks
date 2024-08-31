7befc249c813a2f008ea3a808a62d9ab2aad99a5
rm40405 <rm404085@gmail.com>
2024-08-31 09:23:23 +0600

    first commit

---
+++ task1.js
@@ -0,0 +1,13 @@
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+let burger = 400;
+if (burger>500){
+    console.log("Free Coke");
+}
+else{
+    console.log("Coke: 30tk");
+}
